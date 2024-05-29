7f81a4e5fea7bfd5182f9e8a6069ce1dc439a824
Dennis Wong <denniswong342@gmail.com>
2024-05-29 00:24:57 -0400

    made changes so that code does not display in terminal, and instead creates a txt file with all the necessary information.

--- index.js
+++ index.js
@@ -3,8 +3,12 @@ const PORT = 8000;
 const express = require('express');
 const cheerio = require('cheerio');
 const axios = require('axios');
+const fs = require('fs');
 
 const app = new express();
+
+// Create a write stream to output the results to a file
+const writeStream = fs.createWriteStream('output.txt', { flags: 'a' }); // 'a' means appending to the file
 
 // Function to fetch politician URLs
 function fetchPoliticianUrls(politicianUrl) {
@@ -33,6 +37,7 @@ function processUrl(url, name) {
             const trades = [];
             const buyorsell = [];
             const companyName = [];
+            const dateTraded = [];
             $('.q-field.issuer-ticker', html).each(function () {
                 let stock = $(this).text().trim();
                 const colonIndex = stock.indexOf(":");
@@ -49,20 +54,24 @@ function processUrl(url, name) {
                 const company = $(this).text();
                 companyName.push({ company });
             });
+            $('.q-td.q-column--txDate', html).each(function () {
+                const date = $(this).text();
+                dateTraded.push({ date });
+            });
             // Check if there is a latest trade before logging
             if (trades.length > 0) {
-                console.log(`Latest trade for${name}:`, trades[0], buyorsell[0], companyName[0]);
+                writeStream.write(`Latest trade for ${name} at time: ${JSON.stringify(trades[0])}, ${JSON.stringify(buyorsell[0])}, ${JSON.stringify(companyName[0])}, ${JSON.stringify(dateTraded[0])}\n`);
             } else {
-                console.log(`There is no latest trade for ${name} (${url}).`);
+                writeStream.write(`There is no latest trade for ${name} (${url}).\n`);
             }
         })
         .catch(err => {
-            console.error(`Error processing ${name} (${url}):`, err);
+            writeStream.write(`Error processing ${name} (${url}): ${err}\n`);
         });
 }
 
 // Fetch politician URLs and process them
-fetchPoliticianUrls('https://www.capitoltrades.com/politicians')
+fetchPoliticianUrls('https://www.capitoltrades.com/politicians?page=1')
     .then(urlMap => {
         // Process each URL using Promise.all
         const promises = Array.from(urlMap.entries()).map(([url, name]) => processUrl(url, name));
